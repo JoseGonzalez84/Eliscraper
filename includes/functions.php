7b86e1e76525698c0deb30dbe6397ff5fe280684
Jose Gonzalez <josegs84@gmail.com>
2021-03-01 22:43:29 +0100

    Subida con cambios

--- includes/functions.php
+++ includes/functions.php
@@ -33,14 +33,33 @@
  */
 
 function showHelp(bool $return = true) {
-    $output = "=================================================================\n";
+
+    global $appVersion;
+    
+    $output = "================================================================================\n";
     $output .= "_____ ____   ___    _______________     ___   ____ _____  ____   \n";
     $output .= "| ___\\\\  |   | |   / ___|  ___/  _ \   / _ \ | __ \| ___\/  _ \  \n";
     $output .= "| |___ | |   | |  / /__ | |   | |_\ \ / / \ \| |/ /| |_  | |_\ \ \n";
     $output .= "| ___| | |   | |  \___ \| |   |  _  / | |_| ||  _/ | __| |  _  / \n";
     $output .= "| |___ | |__ | |  ___/ /| |___| | \ \ |  _  || |   | |___| | \ \ \n";
     $output .= "|____| |____\\\\_| |____/ |_____|_|  \_\|_| |_||_|   |_____|_|  \_\\\n";
-    $output .= "=================================================================\n";
+    $output .= "================================================================================\n";
+    $output .= "v{$appVersion}";
+    $output .= "\n";
+    $output .= "Usage:\n";
+    $output .= "--------------------------------------------------------------------------------\n";
+    $output .= " $ php main.php <param:value>\n";
+    $output .= "--------------------------------------------------------------------------------\n";
+    $output .= " · -h        : Show this help screen.\n";
+    $output .= " · -o <type> : REQUIRED. Output type. Type can be `json`, `csv` or `db`.\n";
+    $output .= " · -r <path> : REQUIRED. Recipe file. Name of recipe file in `recetas` folder.\n";
+    $output .= "\n";
+    $output .= "Example\n";
+    $output .= "--------------------------------------------------------------------------------\n";
+    $output .= " $ php main.php -o csv -r powerplanetmoviles\n";
+    $output .= "--------------------------------------------------------------------------------\n";
+    $output .= "\n";
+
     echo $output;
 }
 
@@ -70,7 +89,68 @@ function buildOutput(string $type, string $file, array $content) {
 
 
 /**
+ * Manages the arguments received
+ * 
+ * @param array   $arguments  ARGV received.
+ * 
+ * @return object $parameters Object with the parameters handled.
+ */
+function handleArguments(array $arguments) {
+    $parameters = new stdClass;
+    for ($i=1; $i < count($arguments) ; $i++) { 
+        if ($i % 2 !== 0) {
+            switch (strtolower($arguments[$i])) {
+                case "-o":
+                    // -o: Output type
+                    $i++;
+                    if (empty($arguments[$i]) === false) {
+                        $parameters->outputType = $arguments[$i];
+                        printLine(sprintf('Se ha elegido salida %s', strtoupper($parameters->outputType)), 'info', 5);
+                    } else {
+                        printLine('No se ha definido una salida. Revise el parámetro.', 'error', 1);
+                    }
+                break;
+                case "-r":
+                    // -r: Recipe file
+                    $i++;
+                    if (empty($arguments[$i]) === false) {
+                        $parameters->recipe = $arguments[$i];
+                        printLine(sprintf('Se va a cargar la receta %s', $parameters->recipe), 'info', 5);    
+                    } else {
+                        printLine('No se ha indicado una receta correcta. Revise el parámetro.', 'error', 1);
+                        exit(20);
+                    }
+                break;
+                case "-h":
+                    // -h: Show help
+                    showHelp();
+                    exit(0);
+                break;
+                default:
+                    // The parameter does not exists.
+                    if (preg_match('/(\-{1}[A-z])/', $arguments[$i]) !== false) {
+                        $errorOutput = 'Alguno de los parámetros no fueron correctamente informados.';
+                    } else {
+                        $errorOutput = sprintf('Parámetro incorrecto [%s]', $arguments[$i]);
+                    }
+                    printLine($errorOutput, 'error', 1);
+                    showHelp();
+                    exit(10);         
+                break;
+            }
+        }
+    }
+
+    return $parameters;
+}
+
+
+/**
  * Checks if defined type by user is allowed
+ * 
+ * @param string $type Output type.
+ * 
+ * @return boolean True if is allowed.
  */
 function checkAllowedOutputTypes(string $type){
     $allowedTypes = [
@@ -156,28 +236,50 @@ function strColor(string $str,string $fgcolor='white', string $bgcolor=null)
     return $out;
 }
 
+/**
+ * Prints a formatted line
+ * 
+ * @param string  $string String to show.
+ * @param string  $type   Type of message.
+ * @param integer $level  Level of verbosity. Higher is more verbosity. 1 lowest 5 higher.
+ * @param string  $return If true, the string is returned.
+ * 
+ * @return string
+ */
+function printLine(string $string, string $type='neutral', int $level=3, bool $return=false) {
 
-function printLine(string $string, string $type='neutral', bool $return=false) {
-    switch ($type) {
-        default:
-        case 'neutral':
-            $header = '';
-        break;
+    global $appConfiguration;
 
-        case 'error':
-            $header = strColor(' [FAIL] ', 'white', 'red');
-        break;
-
-        case 'warning':
-            $header = strColor(' [WARN] ', 'black', 'yellow');
-        break;
-
-        case 'success':
-            $header = strColor(' [ OK ] ', 'black', 'green');
-        break;
+    // If the configuration file not load properly, the messages will be displayed anyway.
+    if (empty($appConfiguration) === true) {
+        $appConfiguration->main->verbosity = 5;
     }
 
-    $output = sprintf("%s %s\n", $header, $string);
+    // If the level of this message is above configured verbosity, do not display it.
+    if ($appConfiguration->main->verbosity < $level) {
+        $output = '';
+    } else {
+        switch ($type) {
+            case 'error':
+                $header = strColor(' [FAIL] ', 'white', 'red');
+            break;
+    
+            case 'warning':
+                $header = strColor(' [WARN] ', 'black', 'yellow');
+            break;
+    
+            case 'success':
+                $header = strColor(' [ OK ] ', 'black', 'green');
+            break;
+            
+            case 'neutral':
+            default:
+                $header = '';
+            break;
+        }
+    
+        $output = sprintf("%s %s\n", $header, $string);
+    }
 
     if ($return === true) {
         return $output;
--- main.php
+++ main.php
@@ -11,7 +11,6 @@
  * @author   José González <josegs84@gmail.com>
  * @license  GNU General Public License version 3
  * @version  1.0.2
- * @link     Link
  * _____ ____   ___    _______________     ___   ____ _____  ____   
  * | ___\\  |   | |   / ___|  ___/  _ \   / _ \ | __ \| ___\/  _ \  
  * | |___ | |   | |  / /__ | |   | |_\ \ / / \ \| |/ /| |_  | |_\ \ 
@@ -36,104 +35,128 @@
  * ============================================================================
  */
 
+// Definiciones iniciales.
+$appConfiguration = new stdClass;
+$appVersion       = '1.0.3';
 // Cargar dependencias.
 require_once 'includes/functions.php';
-showHelp();
-// Captura los argumentos del programa
-$recipeFile = $argv[1] ?? '';
-$typeOutputFile = $argv[2] ?? '';
 // Fichero de configuración de la aplicación.
 $configurationFileName = "settings.json";
 if (file_exists($configurationFileName) !== true) {
-    printLine('Ha habido un problema con el fichero de configuración. Se detiene el programa', 'error');
+    printLine('Ha habido un problema con el fichero de configuración.', 'error', 1);
     exit(1);
 } else {
     $appConfiguration = json_decode(file_get_contents($configurationFileName));
-    printLine('Se ha cargado correctamente la configuración.', 'success');
+    printLine('Se ha cargado correctamente la configuración.', 'success', 5);
 }
+// Captura los argumentos del programa
+$parameters = handleArguments($argv);
 // Fichero que carga la configuración de la web a capturar.
-if (empty($recipeFile) === true) {
-    printLine('No se ha indicado un fichero válido', 'error');
+if (empty($parameters->recipe) === true) {
+    printLine('No se ha indicado un fichero válido', 'error', 1);
     exit(2);
-} else if (file_exists("recetas/$recipeFile") !== true) {
-    printLine('Ha habido un problema con el fichero de configuración. Se detiene el programa', 'error');   
+} else if (file_exists("recetas/{$parameters->recipe}.json") !== true) {
+    printLine('Ha habido un problema con el fichero de configuración.', 'error', 1);   
     exit(3);
 } else {
-    $recipeFile = "recetas/$recipeFile";
-    $scrapSettings  = json_decode(file_get_contents($recipeFile));
-    printLine(sprintf('Se ha cargado correctamente la receta [%s].', $scrapSettings->name), 'success');
+    $scrapSettings  = json_decode(file_get_contents("recetas/{$parameters->recipe}.json"));
+    printLine(sprintf('Se ha cargado correctamente la receta [%s].', $scrapSettings->name), 'success', 5);
 }
 
-if (checkAllowedOutputTypes($typeOutputFile) === false) {
-    $typeOutputFile = $appConfiguration->main->defaults->outputFile;
-    printLine(sprintf('Se ha establecido el tipo de salida por defecto [%s].', strtoupper($typeOutputFile)), 'warning');
+if (checkAllowedOutputTypes($parameters->outputType) === false) {
+    $parameters->outputType = $appConfiguration->main->defaults->outputFile;
+    printLine(sprintf('Se ha establecido el tipo de salida por defecto [%s].', strtoupper($parameters->outputType)), 'warning', 3);
 }
 
 // Elementos de configuración
-$localMode      = $scrapSettings->localmode ?? true;
-$urlToScrap     = $scrapSettings->url;
-$outputScrap    = $scrapSettings->filehandle . ".txt";
-$inputCSV       = $scrapSettings->filehandle . ".csv";
+$localMode = $scrapSettings->localmode ?? true;
+$tempFile  = "{$appConfiguration->paths->tempDir}/{$scrapSettings->filename}.tmp";
 // Solo obtenemos la información de internet si no estamos en modo local.
 if ($localMode === false) {
-    $dataOrigin = curl_init($urlToScrap);
-    $fileScraped = fopen($outputScrap, "w");
-
-    curl_setopt($dataOrigin, CURLOPT_FILE, $fileScraped);
-    curl_setopt($dataOrigin, CURLOPT_HEADER, 0);
+    try {
+        $dataOrigin = curl_init($scrapSettings->url);
+        $fileScraped = fopen($tempFile, "w");
     
-    curl_exec($dataOrigin);
-    curl_close($dataOrigin);
-    
-    fclose($fileScraped);
+        curl_setopt($dataOrigin, CURLOPT_FILE, $fileScraped);
+        curl_setopt($dataOrigin, CURLOPT_HEADER, 0);
+        
+        curl_exec($dataOrigin);
+        curl_close($dataOrigin);
+        
+        fclose($fileScraped);
+    } catch (\Throwable $th) {
+        printLine('No se ha podido gestionar el fichero temporal', 'error', 1);
+        exit(4);
+    }
 }
 
-// Abrir el fichero y empezar a sacar elementos
-$fileScraped = fopen($outputScrap, 'r') or die("No se ha podido abrir el fichero");
 // Ficha de producto y sus elementos principales
-$productContent = $scrapSettings->product->tab;
-$subContents = $productContent->tagCapture;
-//
-$principalTag = sprintf('<%s',$productContent->tagType);
+$productContent  = $scrapSettings->product->tab;
+$subContents     = $productContent->tagCapture;
+// Tags
+$principalTag    = sprintf('<%s',$productContent->tagType);
 $endPrincipalTag = sprintf('</%s>',$productContent->tagType);
-$principalClass = sprintf('class="%s"', $productContent->tagClass);
-$gotchaElements = [];
-$cnt = 0;
-// Comienza la fiesta.
-while (!feof($fileScraped)) {
-    // Para la iteracion si se acaba el fichero (creo que ya sobra).
-    if (feof($fileScraped)) break;
-    // Captura la linea que toque, limpiando espacios en blanco.
-    $lineContent = trim(fgets($fileScraped));
+$principalClass  = sprintf('class="%s"', $productContent->tagClass);
+$gotchaElements  = [];
+$cnt             = 0;
+// Abrir el fichero y empezar a sacar elementos
+try {
+    $fileScraped = fopen($tempFile, 'r');
+} catch (\Throwable $th) {
+    printLine(sprintf('No se ha podido abrir el fichero de carga temporal: %s', $th->getMessage()), 'error', 1);
+    exit(20);
+}
 
-    if (preg_match("/$principalTag/", $lineContent) !== false && preg_match("/$principalClass/", $lineContent) !== false) {
-        // Tenemos un nuevo item!
-        $cnt++;
-        $gotchaElements[$cnt] = [];
-        while(!feof($fileScraped)) {
-            // Seguimos capturando líneas.
-            $subLineContent = trim(fgets($fileScraped));
-            // Si llegamos a la etiqueta de cierre del elemento que estemos viendo, se termina el item.
-            if (strpos($subLineContent, $endPrincipalTag) !== false) break;
-            // Comprobamos si las lineas que van saliendo coinciden con alguno de los elementos que queremos capturar.
-            foreach ($subContents as $classForEvaluate => $data) {
-                $dataForEval = sprintf('class="%s"', $classForEvaluate);
-                if (strpos($subLineContent, $dataForEval) !== false) {
-                    if (empty($data->childCapture) === true) {
-                        // No se ha definido ningun punto de captura, por lo que obtiene el contenido del tag.
-                        $gotchaElements[$cnt][$data->childType] = strip_tags($subLineContent);
-                    } else {
-                        // Se busca el atributo definido y se obtiene el valor del mismo.
-                        $tmpContentToSave = strstr($subLineContent, $data->childCapture);
-                        $tmpContentToSave = strstr($tmpContentToSave, "\"");
-                        $gotchaElements[$cnt][$data->childType] = strstr(substr($tmpContentToSave, 1), "\" ", true);
+try {
+    // Comienza la fiesta.
+    while (!feof($fileScraped)) {
+        // Para la iteracion si se acaba el fichero (creo que ya sobra).
+        if (feof($fileScraped)) break;
+        // Captura la linea que toque, limpiando espacios en blanco.
+        $lineContent = trim(fgets($fileScraped));
+
+        if (preg_match("/$principalTag/", $lineContent) !== false && preg_match("/$principalClass/", $lineContent) !== false) {
+            // Tenemos un nuevo item!
+            $cnt++;
+            $gotchaElements[$cnt] = [];
+            while(!feof($fileScraped)) {
+                // Seguimos capturando líneas.
+                $subLineContent = trim(fgets($fileScraped));
+                // Si llegamos a la etiqueta de cierre del elemento que estemos viendo, se termina el item.
+                if (strpos($subLineContent, $endPrincipalTag) !== false) break;
+                // Comprobamos si las lineas que van saliendo coinciden con alguno de los elementos que queremos capturar.
+                foreach ($subContents as $classForEvaluate => $data) {
+                    $dataForEval = sprintf('class="%s"', $classForEvaluate);
+                    if (strpos($subLineContent, $dataForEval) !== false) {
+                        if (empty($data->childCapture) === true) {
+                            // No se ha definido ningun punto de captura, por lo que obtiene el contenido del tag.
+                            $gotchaElements[$cnt][$data->childType] = strip_tags($subLineContent);
+                        } else {
+                            // Se busca el atributo definido y se obtiene el valor del mismo.
+                            $tmpContentToSave = strstr($subLineContent, $data->childCapture);
+                            $tmpContentToSave = strstr($tmpContentToSave, "\"");
+                            $gotchaElements[$cnt][$data->childType] = strstr(substr($tmpContentToSave, 1), "\" ", true);
+                        }
                     }
                 }
             }
+            printLine('Se ha capturado un nuevo item.', 'success', 5);
         }
+        printLine(sprintf('Se han capturado %s item.', $cnt), 'success', 5);
     }
+} catch (\Throwable $th) {
+    printLine(sprintf('Ha habido un problema leyendo el fichero de carga temporal: %s', $th->getMessage()), 'error', 1);
+    exit(21);
 }
-fclose($fileScraped);
+
+try {
+    fclose($fileScraped);
+} catch (\Throwable $th) {
+    printLine(sprintf('Ha habido un problema cerrando el fichero: %s', $th->getMessage()), 'error', 1);
+    exit(22);
+}
+
+
 // Exportar los datos a un fichero CSV.
-buildOutput($typeOutputFile, $scrapSettings->filehandle, $gotchaElements);
+buildOutput($parameters->outputType, $scrapSettings->filename, $gotchaElements);
 ?>
